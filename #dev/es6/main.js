2ee7250aee6326a1d6bccb8de20dff87d1d4315d
Fedor Sharaputo <sharaputo@icloud.com>
2021-03-27 12:45:48 +0300

    hero section

--- #dev/es6/main.js
+++ #dev/es6/main.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+// Header transform effect
+(function () {
+  const header = document.querySelector('.header');
+  window.onscroll = () => {
+    if (window.pageYOffset > 40) {
+      header.classList.add('active');
+    } else {
+      header.classList.remove('active');
+    }
+  };
+})();
